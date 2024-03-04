a623763a2e3da6c673e4b05a2a53434da717543a
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-03-04 13:09:29 -0500

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from './pages/LoginPage';
+import CardPage from './pages/CardPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" index element={<LoginPage />} />
+      <Route path="/cards" index element={<CardPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+}
+
+// function App() {
+// 	return (
+// 	  <CardPage />
+// 	);
+// }
+  
+
+export default App;
---
+++ frontend/src/components/PageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageTitle()
+{
+	return (
+		<h1 id="title">COP4331 MERN Stack Demo </h1>
+	);
+
+};
+
+export default PageTitle;
