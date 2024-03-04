87baf2e3bb363fa0d181d3e450b8b183a5f0622b
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-03-04 15:00:37 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const path = require("path");
-const PORT = process.env.PORT || 5000;
 const express = require("express");
-app.set ("port", (process.env.PORT || 5000));
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 const app = express();
-
+app.set ("port", (process.env.PORT || 5000));
 // For Heroku deployment
 
 // Server static assets if in production
@@ -24,12 +23,11 @@ if (process.env.NODE_ENV === 'production')
 
 // Your MongoDB connection string
 // const uri = "mongodb+srv://thebeast:COP4331-G6@cop4331-g6-lp.rvnbxnv.mongodb.net/?retryWrites=true&w=majority&appName=COP4331-G6-LP";
+require ('dotenv').config();
 const url = process.env.MONGODB_URI;
 const MongoClient = require("mongodb").MongoClient;
-// const client = new MongoClient(uri);
 const client = new MongoClient(url);
 client.connect(console.log("mongodb connected"));
-
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -105,14 +103,28 @@ app.post("/api/login", async (req, res, next) => {
 	const ret = {
 		id: user ? user._id : -1,
 		firstName: user ? user.FirstName : "",
-		username: user ? user.Username : "",
 		lastName: user ? user.LastName : "",
+		username: user ? user.Username : "",
 		error: error
 	};
 
 	res.status(error ? 500 : 200).json(ret);
 });
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PATCH, DELETE, OPTIONS'
+	);
+	next();
+});
+
+// app.listen(5000); // start Node + Express server on port 5000
 app.listen(PORT, () => 
 {
   console.log('Server listening on port ' + PORT);
