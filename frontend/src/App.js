70c55417728de8b6e125309085a572875062ae85
Jovanny Rebollar <62491276+JovannyReb@users.noreply.github.com>
2024-03-11 10:08:23 -0400

    Login/Sign Up page
    Created UI for login/sign up.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LoginPage from './pages/LoginPage';
-// import CardPage from './pages/CardPage';
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" index element={<LoginPage />} />
-      {/* <Route path="/cards" index element={<CardPage />} /> */}
-    </Routes>
-  </BrowserRouter>
-);
+      <Routes>
+        <Route path="/" index element={<LoginPage />} />
+        {/* <Route path="/cards" index element={<CardPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-// function App() {
-// 	return (
-// 	  <CardPage />
-// 	);
-// }
-  
 
 export default App;
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import PageTitle from '../components/PageTitle';
-import Login from '../components/Login';
+import React from "react";
+import Login from "../components/Login";
+import "../index.css";
+import Page from "../components/GlobeInteractive";
 
 const LoginPage = () => {
+  document.body.style.backgroundColor = "black";
 
-	return (
-		<div>
-			<PageTitle />
-			<Login />
-		</div>
-	);
+  return (
+    <div className="min-h-screen flex items-center overflow-hidden">
+      <div className="flex-1 flex justify-center">
+        <Login />
+      </div>
+      <Page />
+    </div>
+  );
 };
 
 export default LoginPage;
