b1ff8da980c8a1ac667dd9b82814e80d3d09c209
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-04-03 13:46:43 -0400

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	url: "mongodb+srv://thebeast:COP4331-G6@cop4331-g6-lp.rvnbxnv.mongodb.net/?retryWrites=true&w=majority&appName=COP4331-G6-LP",
-	database: "COP4331-G6-LP",
-	imgBucket: "images",
-  };
