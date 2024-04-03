be97add66d6e439b94b13c3d2700f7699e14ba51
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-04-03 14:41:54 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -34,30 +34,30 @@ app.use(bodyParser.json());
 
 let gfs;
 
-client.once('open', () => {
-  // Init stream
-  gfs = Grid(client.db, MongoClient);
-});
+// client.once('open', () => {
+//   // Init stream
+//   gfs = Grid(client.db, MongoClient);
+// });
 
-// Create storage engine
-const storage = new GridFsStorage({
-  url: process.env.MONGODB_URI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'images'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
+// // Create storage engine
+// const storage = new GridFsStorage({
+//   url: process.env.MONGODB_URI,
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         const filename = buf.toString('hex') + path.extname(file.originalname);
+//         const fileInfo = {
+//           filename: filename,
+//           bucketName: 'images'
+//         };
+//         resolve(fileInfo);
+//       });
+//     });
+//   }
+// });
 
 const upload = multer({ 
   storage,
