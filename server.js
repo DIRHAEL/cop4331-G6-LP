54bbc3c35ce5f85b98fd7b37553002469182685b
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-04-07 15:55:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,110 +1,32 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.set("port", process.env.PORT || 5000);
+// For Heroku deployment
 
-const multer = require('multer')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const sharp = require('sharp')
-const bcrypt = require('bcryptjs')
-const sharp = require('sharp')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-// import { uploadFile, deleteFile, getObjectSignedUrl } from './s3.js'
-
-const app = express()
-app.set('port', process.env.PORT || 5000)
-if (process.env.NODE_ENV === 'production') {
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
 	// Set static folder
-	app.use(express.static('frontend/build'))
+	app.use(express.static("frontend/build"));
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 	});
 }
 
-require('dotenv').config()
-const url = process.env.MONGODB_URI;
-const MongoClient = require('mongodb').MongoClient
-const client = new MongoClient(url)
-client.connect(console.log('mongodb connected'))
-app.use(cors())
-app.use(bodyParser.json())
-
-// const storage = multer.memoryStorage()
-// const upload = multer({ storage: storage })
-
-// const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
-
-// app.get('/posts', async (req, res) => {
-// 	try 
-// 	{
-// 		const db = client.db('COP4331-G6-LP');
-// 		const posts = await db.collection('Images').find().sort({ created: -1 }).toArray();
-
-// 		for (let post of posts) {
-// 			post.imageUrl = await getObjectSignedUrl(post.imageName);
-// 		}
-
-// 		res.send(posts);
-// 	} 
-// 	catch (e) 
-// 	{
-// 		console.error(e);
-// 		res.status(500).send('An error occurred while fetching posts.');
-// 	}
-// });
-
-
-// app.post('/api/posts', upload.single('image'), async (req, res) => {
-//     try {
-//         const file = req.file;
-//         const caption = req.body.caption;
-//         const imageName = generateFileName();
-
-//         const fileBuffer = await sharp(file.buffer)
-//             .resize({ height: 1920, width: 1080, fit: "contain" })
-//             .toBuffer();
-
-//         await uploadFile(fileBuffer, imageName, file.mimetype);
-
-//         const db = client.db('COP4331-G6-LP');
-//         const post = await db.collection('Images').insertOne({
-//             imageName,
-//             caption,
-//         });
-
-//         res.status(201).send(post);
-//     } catch (e) {
-//         console.error(e);
-//         res.status(500).send('An error occurred while creating the post.');
-//     }
-// });
-
-
-// app.delete("/api/posts/:id", async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const db = client.db('COP4331-G6-LP');
-//         const postsCollection = db.collection('Images');
-
-//         // Find the post
-//         const post = await postsCollection.findOne({ _id: new mongodb.ObjectID(id) });
-//         if (!post) {
-//             return res.status(404).send('Post not found.');
-//         }
-
-//         // Delete the image from S3
-//         await deleteFile(post.imageName);
-
-//         // Delete the post from the database
-//         await postsCollection.deleteOne({ _id: new mongodb.ObjectID(id) });
-
-//         res.send(post);
-//     } catch (e) {
-//         console.error(e);
-//         res.status(500).send('An error occurred while deleting the post.');
-//     }
-// });
+// Your MongoDB connection string
+// const uri = "mongodb+srv://thebeast:COP4331-G6@cop4331-g6-lp.rvnbxnv.mongodb.net/?retryWrites=true&w=majority&appName=COP4331-G6-LP";
+require("dotenv").config();
+const url = process.env.MONGODB_URI; // storing into environmental
+const MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(url);
+client.connect(console.log("mongodb connected"));
+app.use(cors());
+app.use(bodyParser.json());
 
 
 app.post("/api/createuser", async (req, res, next) => {
