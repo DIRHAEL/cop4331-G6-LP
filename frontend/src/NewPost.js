2b5e1ac5c2328f82247f752b57f0248c5af09bf7
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-04-07 14:18:46 -0400

    Add files via upload

---
+++ frontend/src/NewPost.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function NewPost() 
+{
+	const [file, setFile] = useState()
+	const [caption, setCaption] = useState("")
+
+	const submit = async event => 
+	{
+		event.preventDefault()
+
+		const formData = new FormData();
+		formData.append("image", file)
+		formData.append("caption", caption)
+		await axios.post("/api/images", formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+	}
+
+	return (
+		<form onSubmit={submit}>
+			<input onChange={e => setFile(e.target.files[0])} type="file" accept="image/*"></input>
+			<input value={caption} onChange={e => setCaption(e.target.value)} type="text" placeholder='Caption'></input>
+			<button type="submit">Submit</button>
+		</form>
+	)
+}
