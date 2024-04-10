b59135511bb227b5efe72eda13793076afa506cc
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-04-10 15:20:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,17 +15,7 @@ const { uploadFile, deleteFile, getObjectSignedUrl } = require('./s3.js');
 
 const app = express();
 app.set("port", process.env.PORT || 5000);
-// For Heroku deployment
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-	// Set static folder
-	app.use(express.static("frontend/build"));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-	});
-}
 
 // Your MongoDB connection string
 // const uri = "mongodb+srv://thebeast:COP4331-G6@cop4331-g6-lp.rvnbxnv.mongodb.net/?retryWrites=true&w=majority&appName=COP4331-G6-LP";
@@ -39,13 +29,6 @@ client.connect(console.log("mongodb connected"));
 app.use(cors());
 app.use(bodyParser.json());
 
-// Serve static assets (React application)
-app.use(express.static(path.join(__dirname, 'frontend/public')));
-
-// Handle requests to unknown routes by serving the React application
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/public', 'index.html'));
-});
 
 // Set up email transporter
 let transporter = nodemailer.createTransport({
@@ -395,11 +378,24 @@ app.post("/api/login", async (req, res, next) => {
 		firstName: user ? user.FirstName : "",
 		lastName: user ? user.LastName : "",
 		username: user ? user.Username : "",
+		validated: user ? user.Validated : false,
 		error: error,
 	};
 
 	res.status(error ? 500 : 200).json(ret);
 });
+
+// For Heroku deployment
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("frontend/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
+}
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
