e0c31bf3eb903e8af1a6e1cb5402d248c05bc1af
Jovanny Rebollar <62491276+JovannyReb@users.noreply.github.com>
2024-06-07 23:36:49 -0400

    Password checker ui update
    checks for passwork requirements.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,6 +5,10 @@ import * as Tabs from "@radix-ui/react-tabs";
 import * as Form from "@radix-ui/react-form";
 import { useToast } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ReactComponent as EyeCloseIcon } from "../assets/eye-none.svg";
+import { ReactComponent as EyeOpenIcon } from "../assets/eye-open.svg";
+import { ReactComponent as CircleIcon } from "../assets/circle.svg";
+import { ReactComponent as CheckIcon } from "../assets/circle-check.svg";
 
 import { buildPath } from "./buildPath";
 
@@ -24,9 +28,63 @@ const Login = () => {
 
   const [message, setMessage] = useState("");
 
+  // For password icon
+  const [type, setType] = useState("password");
+
+  // Validation states
+  const [lowerValidated, setLowerValidated] = useState(false);
+  const [upperValidated, setUpperValidated] = useState(false);
+  const [numberValidated, setNumberValidated] = useState(false);
+  const [specialValidated, setSpecialValidated] = useState(false);
+  const [lengthValidated, setLengthValidated] = useState(false);
+
   function isValidPassword(password) {
     const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
     return regex.test(password);
+  }
+
+  function handleChange(value) {
+    // regex
+    const lower = new RegExp("(?=.*[a-z])");
+    const upper = new RegExp("(?=.*[A-Z])");
+    const number = new RegExp("(?=.*[0-9])");
+    const special = new RegExp("(?=.*[!@#$%^&*])");
+    const length = new RegExp("(?=.{8,})");
+
+    // lowercase validation
+    if (lower.test(value)) {
+      setLowerValidated(true);
+    } else {
+      setLowerValidated(false);
+    }
+
+    // uppercase validation
+    if (upper.test(value)) {
+      setUpperValidated(true);
+    } else {
+      setUpperValidated(false);
+    }
+
+    // number validation
+    if (number.test(value)) {
+      setNumberValidated(true);
+    } else {
+      setNumberValidated(false);
+    }
+
+    // special validation
+    if (special.test(value)) {
+      setSpecialValidated(true);
+    } else {
+      setSpecialValidated(false);
+    }
+
+    // length
+    if (length.test(value)) {
+      setSpecialValidated(true);
+    } else {
+      setSpecialValidated(false);
+    }
   }
 
   function forgotPassword() {
@@ -36,7 +94,7 @@ const Login = () => {
   return (
     <ChakraProvider>
       <Tabs.Root
-        className="flex flex-col w-[300px] shadow-[0_2px_10px]"
+        className="flex flex-col w-auto shadow-[0_2px_10px]"
         defaultValue="tab1"
       >
         <Tabs.List
@@ -139,7 +197,7 @@ const Login = () => {
               </div>
               <Form.Control asChild>
                 <input
-                  className="box-border bg-black w-full shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-[#5B625F] focus:text-purple-400 shadow-[0_0_0_1px] outline-none  "
+                  className="custom-input"
                   type="email"
                   required
                   onChange={(e) => {
@@ -162,7 +220,7 @@ const Login = () => {
               </div>
               <Form.Control asChild>
                 <input
-                  className="box-border bg-black w-full shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-[#5B625F] focus:text-purple-400 shadow-[0_0_0_1px] outline-none"
+                  className="custom-input"
                   type="password"
                   required
                   onChange={(e) => {
@@ -189,7 +247,7 @@ const Login = () => {
             To use Memory Map enter your details
           </p>
           <Form.Root
-            className="w-[260px]"
+            className="sm:w-fit md:w-[600px]"
             onSubmit={async (event) => {
               event.preventDefault();
 
@@ -268,133 +326,194 @@ const Login = () => {
               }
             }}
           >
-            <Form.Field className="grid mb-[10px]" name="email">
-              <div className="flex items-baseline justify-between">
-                <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-                  First Name
-                </Form.Label>
-                <Form.Message
-                  className="text-[13px] text-white opacity-[0.8]"
-                  match="valueMissing"
-                >
-                  Please enter your first name
-                </Form.Message>
-              </div>
-              <Form.Control asChild>
-                <input
-                  className="box-border bg-black w-full shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-[#5B625F] focus:text-purple-400 shadow-[0_0_0_1px] outline-none"
-                  type="text"
-                  required
-                  onChange={(e) => {
-                    setFirst(e.target.value);
-                  }}
-                />
-              </Form.Control>
-            </Form.Field>
-            <Form.Field className="grid mb-[10px]" name="question">
-              <div className="flex items-baseline justify-between">
-                <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-                  Last Name
-                </Form.Label>
-                <Form.Message
-                  className="text-[13px] text-white opacity-[0.8]"
-                  match="valueMissing"
-                >
-                  Please provide a last name
-                </Form.Message>
-              </div>
-              <Form.Control asChild>
-                <input
-                  className="box-border bg-black w-full shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-[#5B625F] focus:text-purple-400 shadow-[0_0_0_1px] outline-none"
-                  type="text"
-                  required
-                  onChange={(e) => {
-                    setLast(e.target.value);
-                  }}
-                />
-              </Form.Control>
-            </Form.Field>
-            <Form.Field className="grid mb-[10px]" name="question">
-              <div className="flex items-baseline justify-between">
-                <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-                  Username
-                </Form.Label>
-                <Form.Message
-                  className="text-[13px] text-white opacity-[0.8]"
-                  match="valueMissing"
-                >
-                  Please provide a username
-                </Form.Message>
-              </div>
-              <Form.Control asChild>
-                <input
-                  className="box-border bg-black w-full shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-[#5B625F] focus:text-purple-400 shadow-[0_0_0_1px] outline-none"
-                  type="text"
-                  onChange={(e) => {
-                    setUserName(e.target.value);
-                  }}
-                  required
-                />
-              </Form.Control>
-            </Form.Field>
-            <Form.Field className="grid mb-[10px]" name="email">
-              <div className="flex items-baseline justify-between">
-                <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-                  Email
-                </Form.Label>
-                <Form.Message
-                  className="text-[13px] text-white opacity-[0.8]"
-                  match="valueMissing"
-                >
-                  Please enter your email
-                </Form.Message>
-                <Form.Message
-                  className="text-[13px] text-white opacity-[0.8]"
-                  match="typeMismatch"
-                >
-                  Please provide a valid email
-                </Form.Message>
-              </div>
-              <Form.Control asChild>
-                <input
-                  className="box-border bg-black w-full shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-[#5B625F] focus:text-purple-400 shadow-[0_0_0_1px] outline-none  "
-                  type="email"
-                  required
-                  onChange={(e) => {
-                    setSignUpEmail(e.target.value);
-                  }}
-                />
-              </Form.Control>
-            </Form.Field>
-            <Form.Field className="grid mb-[10px]" name="question">
-              <div className="flex items-baseline justify-between">
-                <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-                  Password
-                </Form.Label>
-                <Form.Message
-                  className="text-[13px] text-white opacity-[0.8]"
-                  match="valueMissing"
-                >
-                  Please provide a password
-                </Form.Message>
-              </div>
+            <div className="grid grid-cols-2 gap-x-10">
+              <Form.Field className="grid mb-[10px]" name="firstName">
+                <div className="input-heading">
+                  <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
+                    First Name
+                  </Form.Label>
+                  <Form.Message
+                    className="text-[13px] text-white opacity-[0.8]"
+                    match="valueMissing"
+                  >
+                    Please enter your first name
+                  </Form.Message>
+                </div>
+                <Form.Control asChild>
+                  <input
+                    className="custom-input"
+                    type="text"
+                    required
+                    onChange={(e) => {
+                      setFirst(e.target.value);
+                    }}
+                  />
+                </Form.Control>
+              </Form.Field>
+              <Form.Field className="grid mb-[10px]" name="lastName">
+                <div className="input-heading">
+                  <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
+                    Last Name
+                  </Form.Label>
+                  <Form.Message
+                    className="text-[13px] text-white opacity-[0.8]"
+                    match="valueMissing"
+                  >
+                    Please provide a last name
+                  </Form.Message>
+                </div>
+                <Form.Control asChild>
+                  <input
+                    className="custom-input"
+                    type="text"
+                    required
+                    onChange={(e) => {
+                      setLast(e.target.value);
+                    }}
+                  />
+                </Form.Control>
+              </Form.Field>
+              <Form.Field className="grid mb-[10px]" name="userName">
+                <div className="input-heading">
+                  <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
+                    Username
+                  </Form.Label>
+                  <Form.Message
+                    className="text-[13px] text-white opacity-[0.8]"
+                    match="valueMissing"
+                  >
+                    Please provide a username
+                  </Form.Message>
+                </div>
+                <Form.Control asChild>
+                  <input
+                    className="custom-input"
+                    type="text"
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
+                    required
+                  />
+                </Form.Control>
+              </Form.Field>
+              <Form.Field className="grid mb-[10px]" name="userEmail">
+                <div className="input-heading">
+                  <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
+                    Email
+                  </Form.Label>
+                  <Form.Message
+                    className="text-[13px] text-white opacity-[0.8]"
+                    match="valueMissing"
+                  >
+                    Please enter your email
+                  </Form.Message>
+                  <Form.Message
+                    className="text-[13px] text-white opacity-[0.8]"
+                    match="typeMismatch"
+                  >
+                    Please provide a valid email
+                  </Form.Message>
+                </div>
+                <Form.Control asChild>
+                  <input
+                    className="custom-input"
+                    type="email"
+                    required
+                    onChange={(e) => {
+                      setSignUpEmail(e.target.value);
+                    }}
+                  />
+                </Form.Control>
+              </Form.Field>
+              <Form.Field className="grid mb-[10px]" name="userPassword">
+                <div className="input-heading">
+                  <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
+                    Password
+                  </Form.Label>
+                  <Form.Message
+                    className="text-[13px] text-white opacity-[0.8]"
+                    match="valueMissing"
+                  >
+                    Please provide a password
+                  </Form.Message>
+                </div>
 
-              <Form.Control asChild>
-                <input
-                  className="box-border bg-black w-full shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-[#5B625F] focus:text-purple-400 shadow-[0_0_0_1px] outline-none"
-                  type="password"
-                  required
-                  onChange={(e) => {
-                    setSignUpPassword(e.target.value);
-                  }}
-                />
-              </Form.Control>
-            </Form.Field>
-            <Form.Submit asChild>
-              <button className="box-border w-full text-violet11 shadow-blackA4 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-purple-400 hover:bg-purple-600 px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
-                Submit
-              </button>
-            </Form.Submit>
+                <div className="relative">
+                  <Form.Control asChild>
+                    <input
+                      required
+                      className="custom-input pr-[35px]"
+                      type={type}
+                      onChange={(e) => {
+                        handleChange(e.target.value);
+                      }}
+                    />
+                  </Form.Control>
+
+                  {type === "password" ? (
+                    <span
+                      className="absolute right-[10px] top-[10px]"
+                      onClick={() => setType("text")}
+                    >
+                      <EyeCloseIcon className="text-purple-400 hover:cursor-pointer" />
+                    </span>
+                  ) : (
+                    <span
+                      className="absolute right-[10px] top-[10px]"
+                      onClick={() => setType("password")}
+                    >
+                      <EyeOpenIcon className="text-purple-400 hover:cursor-pointer" />
+                    </span>
+                  )}
+                </div>
+
+                <div className="text-white m-3 ">
+                  <div
+                    className={`flex items-center ${
+                      lowerValidated ? "validated" : "text-red-500"
+                    }`}
+                  >
+                    {lowerValidated ? (
+                      <span className="inline-flex">
+                        <CheckIcon />
+                      </span>
+                    ) : (
+                      <span className="inline-block">
+                        <CircleIcon />
+                      </span>
+                    )}
+                    At least one lowercase letter
+                  </div>
+                  <div
+                    className={upperValidated ? "validated" : "not-validated"}
+                  >
+                    At least one uppercase letter
+                  </div>
+                  <div
+                    className={numberValidated ? "validated" : "not-validated"}
+                  >
+                    At least one one number
+                  </div>
+                  <div
+                    className={specialValidated ? "validated" : "not-validated"}
+                  >
+                    At least one special character
+                  </div>
+                  <div
+                    className={lengthValidated ? "validated" : "not-validated"}
+                  >
+                    At least 8 characters
+                  </div>
+                </div>
+              </Form.Field>
+            </div>
+            <div className="w-full flex justify-end">
+              <Form.Submit>
+                <button className="w-44 box-border text-violet11 shadow-blackA4 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-purple-400 hover:bg-purple-600 px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
+                  Submit
+                </button>
+              </Form.Submit>
+            </div>
           </Form.Root>
         </Tabs.Content>
       </Tabs.Root>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        validated: "rgba(255,255,255,0.5)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      const newComponents = {
+        ".custom-input": {
+          "@apply box-border bg-black w-full h-[35px] appearance-none rounded-[4px] px-[10px] text-white leading-none focus:text-purple-400 shadow-[0_0_0_1px] outline-none":
+            {},
+        },
+        ".input-heading": {
+          "@apply flex-col md:flex-row flex items-baseline justify-between": {},
+        },
+        ".validated": {
+          "@apply text-validated": {},
+        },
+      };
+
+      addComponents(newComponents);
+    },
+  ],
 };
